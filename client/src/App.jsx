6bb3d0d9ecb4fc708a5268c9e4819e3a4f28bd44
Thamir <thamirpkd004@gmail.com>
2024-05-29 11:53:53 +0530

    Created Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <>
+    <div className='flex flex-col'>
     <Header/>
-	    <div className='text-green-700 text-3xl text-center'>Hello World!</div>
+      <div className="h-[calc(100vh-4rem)] w-screen">
+      <div className='text-green-700 text-3xl text-center'>Hello World!</div>
+      </div>
       <Footer/>
-    </>
+    </div>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='h-15 w-screen flex px-8 py-4 justify-between items-center border-b border-gray-400'>
+    <div className='h-16 w-screen flex px-8 py-4 justify-between items-center border-b border-gray-400'>
           <img className='h-10' src="https://en.wikichip.org/w/images/thumb/a/a9/Amazon_logo.svg/603px-Amazon_logo.svg.png" alt="logo" />
           <div className="w-1/3 flex justify-evenly font-semibold  text-xl">
             <p>Home</p>
