07e74394b9ff72053d82c99a4c3fbfa9668c411e
Thamir <thamirpkd004@gmail.com>
2024-06-01 10:08:56 +0530

    Removed some unwanted code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,9 @@
-import React, { useContext, useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import { UserContext } from '../context/UserContext';
+import React, {useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { setUser } = useContext(UserContext);
 
   const handleSubmit = async(e) => {
     e.preventDefault();
